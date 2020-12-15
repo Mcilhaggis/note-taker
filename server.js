ccb7e25db8c883fb277672e57415b05b3fe08af2
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-12-15 14:40:37 -0500

    adding unique ID and delete functionality

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const express = require('express');
 const app = express();
 const fs = require('fs');
 
 const PORT = 3003;
+//this allows the use of the CSS and Js files
+app.use(express.static('public'));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +12,6 @@ app.use(express.json());
 
 
 //Setting up the HTML routes
-
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/public/index.html")
 })
@@ -23,44 +23,54 @@ app.get("/notes", function(req, res){
 
 //Setting up the API routes
 
-//Set up the notes variable
-//* GET `/api/notes` - Reads the `db.json` file and return all saved notes as JSON.
-fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
+//Set up the noteList variable
+//* GET `/api/notes` - Reads the `db.json` file and returns all saved notes as JSON.
+let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+// console.log(noteList)
 
-    var notes = JSON.parse(data);
-    console.log(notes)
+//Sets up the /api/notes get route - displays the saved notes
+app.get("/api/notes", function(req, res) {res.json(noteList)} );
 
-    //Sets up the /api/notes get route
-    app.get("/api/notes", function(req, res){
-    res.json(notes);
-    });
-});
-
-//POST /api/notes save on the request body, add it to the `db.json` file, and then return the new note to the client.
-
+//POST /api/notes save on the request body, adds it to the `db.json` file, and then return the new notes to the client.
 app.post('/api/notes', (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    //Get the value of the last id if it exists
-    let lastId;
-    if(notes.length){
-        lastId = Math.max(...(noteList.map(note => note.id)));
-    } else {
-        lastId = 0;
-        }
 
-    console.log(lastId);
-    //Starts the id's at 1
-    const id = lastId + 1;
+    // req.body hosts is equal to the JSON post sent from the user, storing its value in a variable
+    const newNote = req.body;
+    //Assigning a unique ID to the note to allow for call back
+    unqiueID();
+    // console.log(newNote)
 
-    // pushes the id of the note along with the rest of the text/input of the array in the request.body
-    notes.push({ id, ...req.body });
-    //Removes last index
-    res.json(noteList.slice(-1));
+    noteList.push(newNote); //add the note data to the note list
+
+        //Append new note to the json file
+    fs.writeFile('db/db.json', JSON.stringify(noteList), 'utf8', function(err){
+        if (err) throw err;
+        res.json(newNote);
+        });
     });
 
+    const unqiueID = () => {
+        for (i = 0; i < noteList.length; i++) {
+            noteList[i].id = i + 1;
+            // console.log(noteList[i])
+        }
+    };
 
 
+//`DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete. 
+// Displays a single character, or returns false
+app.delete('/api/notes/:id', (req, res) => {
+    //filter out the note with the id clicked
+    notes = noteList.filter(n => n.id != req.params.id)
+    // console.log(notes)
+
+    //rewrite the json file with the note with that particular id removed
+    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+       if (err) throw err; 
+       res.json(notes);
+
+    });
+  });
 
 
 app.listen(PORT, function(){
