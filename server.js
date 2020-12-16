8f32fa877a75ea67191272ea878a02e59f597ea6
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-12-15 22:39:50 -0500

    adding heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 //this allows the use of the CSS and Js files
 app.use(express.static('public'));
 
